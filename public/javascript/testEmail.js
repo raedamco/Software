b13e5768e3b7ee61c336ff4b3d773af18610f4bf
Omar <waked@icloud.com>
2020-05-08 23:50:40 -0700

    Revert

--- public/javascript/testEmail.js
+++
@@ -1,8 +0,0 @@
-//Listen for form submit
-document.getElementById('Contact_form').addEventListener('submit', submitForm);
-
-function submitForm(e)
-{
-  e.preventDefault();
-    formToDatabase();
-}
