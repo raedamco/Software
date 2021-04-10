ba069024bbc2e0a67fc9c712c4bc0133870c56ae
Ryan Kirkpatrick <rk168810@gmail.com>
2021-04-10 15:25:51 -0700

    Setup index and copied files

--- business/src/index.js
+++ business/src/index.js
@@ -1,14 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import * as firebase from "firebase";
+
+var firebaseConfig = {
+	apiKey: "AIzaSyCKghNDOPOufY-8SYVGW4xpOeZC3fDVZko",
+	authDomain: "theory-parking.firebaseapp.com",
+	databaseURL: "https://theory-parking.firebaseio.com",
+	projectId: "theory-parking",
+	storageBucket: "theory-parking.appspot.com",
+	messagingSenderId: "192548003681",
+	appId: "1:192548003681:web:1f092e58a62891359caf20",
+	measurementId: "G-D9YWC0BFVD",
+};
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Router>
+			<App />
+		</Router>
+	</React.StrictMode>,
+	document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
