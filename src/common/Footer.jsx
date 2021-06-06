fc5e11f9fe4ef71bd3ac4121fdd42ee6b8895c61
Ryan Kirkpatrick <Rk168810@gmail.com>
2021-06-06 16:10:45 -0700

    Added top button html

--- src/common/Footer.jsx
+++ src/common/Footer.jsx
@@ -1,22 +1,29 @@
 const Footer = () => {
 	return (
-		<footer id="tp-footer">
-			<div className="copy">
-				<div className="container">
-					<div className="row">
-						<div
-							className="col-md-12 text-left"
-							style={{ paddingBottom: "50px" }}
-						>
-							<p>
-								Copyright &copy; {new Date().getFullYear()} Raedam Inc. All
-								rights reserved.
-							</p>
+		<>
+			<footer id="tp-footer">
+				<div className="copy">
+					<div className="container">
+						<div className="row">
+							<div
+								className="col-md-12 text-left"
+								style={{ paddingBottom: "50px" }}
+							>
+								<p>
+									Copyright &copy; {new Date().getFullYear()} Raedam Inc. All
+									rights reserved.
+								</p>
+							</div>
 						</div>
 					</div>
 				</div>
+			</footer>
+			<div class="gototop js-top">
+				<a href="#" class="js-gotop">
+					<i class="icon-arrow-up2"></i>
+				</a>
 			</div>
-		</footer>
+		</>
 	);
 };
 
